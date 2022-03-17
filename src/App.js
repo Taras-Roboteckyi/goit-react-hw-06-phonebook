b94c8f6abbb60e28c84b18c738f75c083189fe81
Taras-Roboteckyi <rtvr@ukr.net>
2022-03-17 21:05:23 +0200

    add persist Redux

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@
 import ContactForm from './components/Form/Form';
 import Filter from './components/Filter/Filter';
 import ContactList from './components/ContactList/ContactList';
-//import useLocalStorage from './Hooks/useLocalStorage';
 
 import { Container } from './App.global.styled';
 import { TitlePhoneBook, TitleContacts, Section } from './App.styled';
 
-//const LocalStorageKey = 'contactsKey';
-
 export default function App() {
-  //const [contacts, setContacts] = useLocalStorage(LocalStorageKey, []);
-
   return (
     <Container>
       <Section>
